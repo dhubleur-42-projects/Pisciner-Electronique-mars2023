699016089aa3bf432524320cecbaecdac6ffb9d5
dhubleur <dhubleur@student.42.fr>
2023-03-13 13:50:52 +0100

    ex04

---
+++ day00/ex04/main.c
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void wait_ms(int ms) {
+	for (int i = 0; i < ms; i++) {
+		_delay_ms(1);
+	}
+}
+
+int main() {
+
+	//PB0 = led
+	//PD2 = button
+
+	//Initialize the PB0 pin as an output
+	DDRB = (1 << PB0);
+
+	//Initialize the PD2 pin as an input
+	DDRD = (0 << PD2);
+
+	int buttonState = 0;
+
+	while (1) {
+		int readState = !(PIND & (1 << PD2));
+		if(buttonState == 0 && readState == 1) {
+			PORTB ^= (1 << PB0);
+			buttonState = 1;
+			wait_ms(20);
+		}
+		else if(buttonState == 1 && readState == 0) {
+			buttonState = 0;
+			wait_ms(20);
+		}
+	}
+
+	return (0);
+}
