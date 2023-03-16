43907a5d5684829f30b622cad1f5d61ee9a817e2
dhubleur <dhubleur@student.42.fr>
2023-03-16 13:40:09 +0100

    ex02

---
+++ day03/ex02/main.c
@@ -0,0 +1,74 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#ifndef F_CPU
+# define F_CPU 16000000UL
+#endif
+
+#define UART_BAUDRATE 115200
+
+//Intitialize the UART
+void uart_init() {
+	//Set baud rate
+	UBRR0L = (( F_CPU / 8 / UART_BAUDRATE ) - 1) / 2;
+
+	//Enable receiver and transmitter
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+
+	//Set frame format: 8data, 1stop bit
+	UCSR0C = (1<<USBS0) | (3<<UCSZ00);
+}
+
+//Write a character to PC serial port
+void uart_tx(char c) {
+	//Wait for empty transmit buffer
+	while (!( UCSR0A & (1<<UDRE0)));
+
+	//Put data into buffer, sends the data
+	UDR0 = c;
+}
+
+void uart_printstr(char *str) {
+	while (*str) {
+		uart_tx(*str);
+		str++;
+	}
+}
+
+//Read a character from PC serial port
+char uart_rx(void) {
+	//Wait for data to be received
+	while (!(UCSR0A & (1<<RXC0)));
+
+	//Get and return received data from buffer
+	return UDR0;
+}
+
+int main() {
+	uart_init();
+	
+	//Initialize the timer
+	TCNT1 = 0;
+
+	//Set the timer to interrupt and clear on overflow
+	TIFR1  = (1 << TOV1);
+	TIMSK1 = (1 << TOIE1);
+
+	//Set the timer1 prescaler to 1024
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+
+	//Set the timer1 to 2s
+	OCR1A = (F_CPU / 1024 - 1) * 1;
+
+	//Enable global interrupts
+	sei();
+
+	while (1) {
+		char c = uart_rx();
+		uart_printstr("You typed: ");
+		uart_tx(c);
+		uart_printstr("\r\n");
+	}
+
+}
