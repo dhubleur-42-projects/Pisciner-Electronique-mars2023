c50b00e5e9db52efc5b58b34110ea7db053688ea
dhubleur <dhubleur@student.42.fr>
2023-03-18 13:58:11 +0100

    ex01

--- day04/ex01/main.c
+++ day04/ex01/main.c
@@ -9,8 +9,14 @@
 #define UART_BAUDRATE 115200
 
 #define AHTR20_ADDR 0x38
+
 #define SCL_FREQUENCY 100000
 #define TWI_BITRATE (((F_CPU / SCL_FREQUENCY) - 16) / 2)
+
+typedef enum e_I2C_MODE {
+	I2C_WRITE = 0,
+	I2C_READ = 1
+} I2C_MODE;
 
 void uart_init() {
 	//Set baud rate
@@ -67,6 +73,21 @@ void print_i2c_status() {
 		case 0x38:
 			uart_printstr("Arbitration lost in SLA+W or data bytes\r\n");
 			break;
+		case 0x40:
+			uart_printstr("SLA+R has been transmitted, ACK has been received\r\n");
+			break;
+		case 0x48:
+			uart_printstr("SLA+R has been transmitted, NACK has been received\r\n");
+			break;
+		case 0x50:
+			uart_printstr("Data has been received, ACK has been returned\r\n");
+			break;
+		case 0x58:
+			uart_printstr("Data has been received, NACK has been returned\r\n");
+			break;
+		case 0x00:
+			uart_printstr("Bus error\r\n");
+			break;
 		default:
 			uart_printstr("Unknown status\r\n");
 			break;
@@ -77,33 +98,78 @@ void wait_i2c() {
 	while (!(TWCR & (1<<TWINT)));
 }
 
-//Init I2C to communicate with AHT20 (U3)
-//Frequency: 100kHz
 void i2c_init(void) {
 	TWBR = TWI_BITRATE;
 	TWSR = 0;
 }
 
-//Start I2C communication in writing mode
-void i2c_start(void) {
+void i2c_start(I2C_MODE mode) {
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	wait_i2c();
-	print_i2c_status();
 
-	TWDR = AHTR20_ADDR;
+	TWDR = AHTR20_ADDR << 1 | mode;
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	wait_i2c();
-	print_i2c_status();
 }
 
-//Interrupt I2C communication
+void i2c_write(unsigned char data) {
+	wait_i2c();
+	TWDR = data;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	wait_i2c();
+}
+
+uint8_t i2c_read(int ack) {
+	TWCR = (1<<TWINT) | (ack<<TWEA) | (1<<TWEN);
+	wait_i2c();
+	return TWDR;
+}
+
+void print_hex_value(uint8_t c) {
+	char hex[] = "0123456789ABCDEF";
+	uart_tx(hex[c >> 4]);
+	uart_tx(hex[c & 0x0F]);
+}
+
 void i2c_stop(void) {
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+	TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
+}
+
+void read_temp_sensor() {
+	i2c_start(I2C_WRITE);
+
+	i2c_write(0xac);
+	i2c_write(0x33);
+	i2c_write(0x00);
+	i2c_stop();
+
+	_delay_ms(80);
+
+	i2c_start(I2C_READ);
+
+	uint8_t read_byte = i2c_read(1);
+	while (read_byte & 0b10000000) {
+		_delay_ms(80);
+		read_byte = i2c_read(1);
+	}
+	print_hex_value(read_byte);
+	uart_tx(' ');
+	
+	for (int i = 0; i < 6; i++) {
+		print_hex_value(i2c_read(i != 5));
+		uart_tx(' ');
+	}
+	i2c_stop();
+
+	uart_printstr("\r\n");
 }
 
 int main() {
 	uart_init();
 	i2c_init();
-	i2c_start();
-	i2c_stop();
+	
+	while (1) {
+		read_temp_sensor();
+		_delay_ms(1000);
+	}
 }
