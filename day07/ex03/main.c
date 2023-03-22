1af70181b5d26aed8f705344e24d9409a33d6512
dhubleur <dhubleur@student.42.fr>
2023-03-22 13:16:08 +0100

    ex03

--- day07/ex03/main.c
+++ day07/ex03/main.c
@@ -20,48 +20,6 @@ void adc_init() {
 	ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
-uint16_t adc_read(int mux3, int mux2, int mux1, int mux0) {
-	//Enable ADC
-	ADCSRA |= (1 << ADEN);
-
-	// Choose ADC channel
-	if (mux0) {
-		ADMUX |= (1 << MUX0);
-	} else {
-		ADMUX &= ~(1 << MUX0);
-	}
-	if (mux1) {
-		ADMUX |= (1 << MUX1);
-	} else {
-		ADMUX &= ~(1 << MUX1);
-	}
-	if (mux2) {
-		ADMUX |= (1 << MUX2);
-	} else {
-		ADMUX &= ~(1 << MUX2);
-	}
-	if (mux3) {
-		ADMUX |= (1 << MUX3);
-	} else {
-		ADMUX &= ~(1 << MUX3);
-	}
-
-	// Start conversion
-	ADCSRA |= (1 << ADSC);
-
-	// Wait for conversion to finish
-	while (ADCSRA & (1 << ADSC));
-
-	// Read ADC value (10 bits)
-	uint16_t res = ADCL;
-	res |= ((ADCH & 0b00000011) << 8);
-
-	// Disable ADC
-	ADCSRA &= ~(1 << ADEN);
-
-	return res;
-}
-
 uint16_t adc_read_chip_temperature() {
 	//Enable ADC
 	ADCSRA |= (1 << ADEN);
